880831b5ab377087cb544206479fa50f48504c5e
luc0liv <luoliveiradacunha@gmail.com>
2022-11-22 16:19:18 -0300

    REQ 1: Testando se links são exibidos na página

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('É exibido na tela um link com o texto Home', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente App.js', () => {
+  it('É exibido na tela um link com o texto Home', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  it('É exibido na tela um link com o texto About', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  it('É exibido na tela um link com o texto Favorite Pokémon', () => {
+    renderWithRouter(<App />);
+    const faveLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    expect(faveLink).toBeInTheDocument();
+  });
+});
