07aeecaffff59d317d6bce0937e3eca74a7d91e9
luc0liv <luoliveiradacunha@gmail.com>
2022-11-22 19:10:27 -0300

    REQ 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found;', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundMessage = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image).toBeInTheDocument();
+  });
+});
