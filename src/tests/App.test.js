fa0acb0c28aef317f9816b756d9d3534a130d4e9
luc0liv <luoliveiradacunha@gmail.com>
2022-11-22 16:31:16 -0300

    REQ 1: Testando direcionamento de páginas

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Teste o componente App.js', () => {
   it('É exibido na tela um link com o texto Home', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
     const homeLink = screen.getByRole('link', { name: 'Home' });
     expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
 
   it('É exibido na tela um link com o texto About', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
     const aboutLink = screen.getByRole('link', { name: 'About' });
     expect(aboutLink).toBeInTheDocument();
+
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
   });
 
   it('É exibido na tela um link com o texto Favorite Pokémon', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
     const faveLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
     expect(faveLink).toBeInTheDocument();
+
+    userEvent.click(faveLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => history.push('/asadsada'));
+
+    const notFound = screen.getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
   });
 });
