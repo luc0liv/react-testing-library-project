dd31f65c1f66bf0ba00176cdd328df640c457df7
luc0liv <luoliveiradacunha@gmail.com>
2022-11-22 17:32:27 -0300

    REQ 2: Testando se existem 2 parágrafos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -10,6 +10,14 @@ describe('Teste se a página contém as informações sobre a Pokédex', () => {
     expect(pokedexHeading).toBeInTheDocument();
   });
 
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraphOne = screen.getByText(/this application simulates a pokédex, a digital encyclopedia containing all pokémon/i);
+    const paragraphTwo = screen.getByText(/one can filter pokémon by type, and see more details for each one of them/i);
+    expect(paragraphOne).toBeInTheDocument();
+    expect(paragraphTwo).toBeInTheDocument();
+  });
+
   it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
     renderWithRouter(<About />);
     const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
