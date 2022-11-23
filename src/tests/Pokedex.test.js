7b90162d701861082d97490ecec203a0126e28f8
luc0liv <luoliveiradacunha@gmail.com>
2022-11-22 21:24:20 -0300

    REQ 5: Testando filtros

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -32,14 +32,7 @@ describe('Teste o componente <Pokedex.js />', () => {
       pokemonList={ pokemonList }
       isPokemonFavoriteById={ favoritePokemons }
     />);
-    // const pikachu = screen.getByText(/pikachu/i);
-    // expect(pikachu).toBeInTheDocument();
-    // const nextButton = screen.getByRole('button', { name: /próximo pokémon/i });
-    // expect(nextButton).toBeInTheDocument();
-    // userEvent.click(nextButton);
-    // const charmander = screen.getByText(/charmander/i);
-    // expect(screen.queryByText(/pikachu/i)).not.toBeInTheDocument();
-    // expect(charmander).toBeInTheDocument();
+
     pokemonList.forEach((pokemon) => {
       const poke = screen.getByText(pokemon.name);
       const nextButton = screen.getByRole('button', { name: /próximo pokémon/i });
@@ -49,11 +42,41 @@ describe('Teste o componente <Pokedex.js />', () => {
     });
   });
 
-  // it('Teste se a Pokédex tem os botões de filtro', () => {
-  //   renderWithRouter(<Pokedex
-  //     pokemonList={ pokemonList }
-  //     isPokemonFavoriteById={ favoritePokemons }
-  //   />);
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList }
+      isPokemonFavoriteById={ favoritePokemons }
+    />);
+    const filterButtons = screen.getAllByTestId('pokemon-type-button');
+    filterButtons.forEach((button) => {
+      expect(button).toBeInTheDocument();
+    });
+  });
 
-  // });
+  it('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos Pokémon daquele tipo', () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList }
+      isPokemonFavoriteById={ favoritePokemons }
+    />);
+
+    const allTypes = screen.getByRole('button', { name: /all/i });
+    const firePokemons = pokemonList.filter((pokemon) => pokemon.type === 'Fire');
+    const fire = screen.getByRole('button', { name: /fire/i });
+
+    expect(screen.queryByText(/charmander/i)).not.toBeInTheDocument();
+    userEvent.click(fire);
+    expect(screen.getByText(/charmander/i)).toBeInTheDocument();
+    expect(allTypes).toBeInTheDocument();
+
+    firePokemons.forEach((poke) => {
+      const next = screen.getByRole('button', { name: /próximo pokémon/i });
+      const firePokemon = screen.getByText(poke.name);
+      userEvent.click(next);
+      expect(firePokemon).toBeInTheDocument();
+    });
+  });
+
+  // it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+
+  // })
 });
